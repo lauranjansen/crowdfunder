6a09dc6ba887582d1c6df80ef2e8079d82813b70
lauranjansen <lauran.jansen@gmail.com>
2015-03-03 16:09:10 -0500

    Initial user controller setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to #
+		else
+			render #
+		end
+	end
+
+	def show
+
+	end
+
+	private
+	def users_params
+		#change for sorcery
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
 end
