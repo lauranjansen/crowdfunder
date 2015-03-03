d4d497131791700954b54f6a1685c08173a8e981
Zachary Toth <zachtoth@gmail.com>
2015-03-03 17:21:53 -0500

    Added all the function to the project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,38 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.all
 	end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.owner = current_user
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new, alert: 'Project could not be created.'
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: 'Project was deleted.'
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title, :description, :funding_goal, :start_date, :end_date)
+  end
+
 end
