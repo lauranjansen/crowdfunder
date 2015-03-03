392668fb1b685ab94c3875e76cc1059208b7fd96
lauranjansen <lauran.jansen@gmail.com>
2015-03-03 17:49:26 -0500

    Add sessions and update index page, seeds and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   end
     
   resources :users
-  resources :sessions 
+  resources :sessions
+
+  get 'login' => 'sessions#new', as: :login
+  post 'logout' => 'sessions#destroy', as: :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
