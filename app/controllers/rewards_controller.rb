d8703bf9964bf83940a44b16406b9f7e0b7d260f
Zachary Toth <zachtoth@gmail.com>
2015-03-04 15:39:54 -0500

    Rewards are now appear

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,16 +1,31 @@
 class RewardsController < ApplicationController
-
+before_filter :load_project
   def show
+    @reward = Reward.find(params[:id])
   end
 
   def create
+    @reward = @product.rewards.build(reward_params)
+    @reward.user = current_user
+
+    if @reward.save
+      redirect_to projects_path, notice: 'Review created successfully'
+    else
+      render new_project_path
+    end
   end
 
   def destroy
   end
 
   private
+  def reward_params
+    params.require(:reward).permit(:title,:description,:amount,:_destroy)
+  end
 
+  def load_project
+    @project = Project.find(params[:project_id])
+  end
 
 
 end
