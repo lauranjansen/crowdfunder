23e3d4f35a0af60ac5dc358fce9a5c7eb8abd68c
jmy19 <jasonmyers@rogers.com>
2015-03-04 19:04:21 -0500

    added firstname, lastname to new user form to add content to show user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:email, :password, :password_confirmation)
+		params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
 	end
 end
